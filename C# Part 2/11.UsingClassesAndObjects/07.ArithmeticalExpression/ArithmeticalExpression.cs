7069b8885030fa1e848f815e1589651673c69bf5
Nikolay Nikolov <nikolov89@gmail.com>
2013-08-04 17:20:04 +0300

    Reversed Polish Notation small fixes

--- C# Part 2/11.UsingClassesAndObjects/07.ArithmeticalExpression/ArithmeticalExpression.cs
+++ C# Part 2/11.UsingClassesAndObjects/07.ArithmeticalExpression/ArithmeticalExpression.cs
@@ -8,291 +8,284 @@ using System.Threading.Tasks;
 
 class ArithmeticalExpression
 {
-    static List<char> operators = new List<char>() { '+', '-', '*', '/' };
-    static List<string> functions = new List<string>() { "ln", "sqrt", "pow" };
-    static List<char> brackets = new List<char>() { '(', ')' };
+    //there are still bugs to be fixed
+
+    static List<string> operators = new List<string>() { "+", "-", "*", "/" };
+    static List<string> functions = new List<string>() { "ln", "pow", "sqrt" };
 
     static void Main()
     {
         Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
         string input = Console.ReadLine();
+        Console.WriteLine(ReversedPolish(ReadTokens(TrimInput(input))));
 
-        string trimmedInput = TrimInput(input);
-        Queue<string> ShuntingYardQueue = ProcessTokens(trimmedInput);
-        Console.WriteLine(ReversePolishNotation(ShuntingYardQueue)); 
     }
-
-    static string ReversePolishNotation(Queue<string> queue)
+    static string ReversedPolish(Queue<string> readTokens)
     {
-        List<string> operate = new List<string>() { "+", "-", "*", "/" };
-        var stack = new Stack<string>();
-        var list = queue.ToList();
-        for (int i = 0; i < list.Count; i++)
+        Stack<string> stack = new Stack<string>();
+        double value;
+        while (readTokens.Count > 0)
         {
-            if (!operate.Contains(list[i]) && !functions.Contains(list[i]))
+            string element = readTokens.Peek();
+            if (double.TryParse(element, out value))
             {
-                stack.Push(list[i]);
+                stack.Push(value.ToString());
+                readTokens.Dequeue();
             }
-
-            if (list[i] == "+")
+            else
             {
-                if (stack.Count < 2)
+                readTokens.Dequeue();
+                if (operators.Contains(element))
                 {
-                    Console.WriteLine("nqma dostatychen broi argumenti");
-                    throw new ArgumentException();
+                    if (stack.Count < 2)
+                    {
+                        throw new ArgumentException("Insufficient values in expression!");
+                    }
+                    else
+                    {
+                        if (element == "+")
+                        {
+                            double result = double.Parse(stack.Pop()) + double.Parse(stack.Pop());
+                            stack.Push(result.ToString());
+                        }
+                        else if (element == "-")
+                        {
+                            double first = double.Parse(stack.Pop());
+                            double second = double.Parse(stack.Pop());
+                            double result = second - first;
+                            stack.Push(result.ToString());
+                        }
+                        else if (element == "*")
+                        {
+                            double result = double.Parse(stack.Pop()) * double.Parse(stack.Pop());
+                            stack.Push(result.ToString());
+                        }
+                        else if (element == "/")
+                        {
+                            double first = double.Parse(stack.Pop());
+                            double second = double.Parse(stack.Pop());
+                            double result = second / first;
+                            stack.Push(result.ToString());
+                        }
+                    }
                 }
+
                 else
                 {
-                    double firstValue = double.Parse(stack.Pop());
-                    double secondValue = double.Parse(stack.Pop());
-                    stack.Push((firstValue + secondValue).ToString());
+                    if (element == "pow")
+                    {
+                        if (stack.Count < 2)
+                        {
+                            throw new ArgumentException("Insufficient values in expression!");
+                        }
+                        else
+                        {
+                            double first = double.Parse(stack.Pop());
+                            double second = double.Parse(stack.Pop());
+                            double result = Math.Pow(second,first);
+                            stack.Push(result.ToString());
+                        }
+                    }
+                    else if (element == "sqrt")
+                    {
+                        if (stack.Count < 1)
+                        {
+                            throw new ArgumentException("Insufficient values in expression!");
+                        }
+                        else
+                        {
+                            double result = Math.Sqrt(double.Parse(stack.Peek()));
+                            stack.Pop();
+                            stack.Push(result.ToString());
+                        }
+                    }
+                    else if (element == "ln")
+                    {
+                        if (stack.Count < 1)
+                        {
+                            throw new ArgumentException("Insufficient values in expression!");
+                        }
+                        else
+                        {
+                            double result = Math.Log(double.Parse(stack.Peek()));
+                            stack.Pop();
+                            stack.Push(result.ToString());
+                        }
+                    }
                 }
             }
-
-            if (list[i] == "-")
-            {
-                if (stack.Count < 2)
-                {
-                    Console.WriteLine("nqma dostatychen broi argumenti");
-                    throw new ArgumentException();
-                }
-                else
-                {
-                    double firstValue = double.Parse(stack.Pop());
-                    double secondValue = double.Parse(stack.Pop());
-                    stack.Push((secondValue - firstValue).ToString());
-                }
-            }
-
-            if (list[i] == "/")
-            {
-                if (stack.Count < 2)
-                {
-                    Console.WriteLine("nqma dostatychen broi argumenti");
-                    throw new ArgumentException();
-                }
-                else
-                {
-                    double firstValue = double.Parse(stack.Pop());
-                    double secondValue = double.Parse(stack.Pop());
-                    stack.Push((secondValue / firstValue).ToString());
-                }
-            }
-
-            if (list[i] == "*")
-            {
-                if (stack.Count < 2)
-                {
-                    Console.WriteLine("nqma dostatychen broi argumenti");
-                    throw new ArgumentException();
-                }
-                else
-                {
-                    double firstValue = double.Parse(stack.Pop());
-                    double secondValue = double.Parse(stack.Pop());
-                    stack.Push((secondValue * firstValue).ToString());
-                }
-            }
-
-            if (list[i] == "ln")
-            {
-                if (stack.Count < 1)
-                {
-                    Console.WriteLine("nqma dostatychen broi argumenti");
-                    throw new ArgumentException();
-                }
-                else
-                {
-                    double firstValue = double.Parse(stack.Pop());
-                    stack.Push((Math.Log(firstValue)).ToString());
-                }
-            }
-
-
-            if (list[i] == "sqrt")
-            {
-                if (stack.Count < 1)
-                {
-                    Console.WriteLine("nqma dostatychen broi argumenti");
-                    throw new ArgumentException();
-                }
-                else
-                {
-                    double firstValue = double.Parse(stack.Pop());
-                    
-                    stack.Push((Math.Sqrt(firstValue)).ToString());
-                }
-            }
-
         }
-        string result = "";
+
+        string atEnd = "";
         if (stack.Count == 1)
         {
-            result = stack.Pop();
+            atEnd = stack.Peek();
+        }
+        else
+        {
+            throw new ArgumentException("Too many valuew in expression!");
         }
 
-        return result;
-
-            
+        return atEnd;
     }
 
-    
-
-    static Queue<string> ProcessTokens(string trimmedInput)
+    static Queue<string> ReadTokens(string str)
     {
         Queue<string> queue = new Queue<string>();
         Stack<string> stack = new Stack<string>();
         var number = new StringBuilder();
-
-        for (int i = 0; i < trimmedInput.Length; i++)
+        // (-1.5) +   (-0.6 + 6) + pow(-1,-5) + ln(1)
+        for (int i = 0; i < str.Length; i++)
         {
-            
-            if (trimmedInput[i] >= '0' && trimmedInput[i] <= '9' || trimmedInput[i] == '-')
+            char token = str[i];
+            // if token is a number ( works for negative and positive)
+            if ((token >= '0' && token <= '9' || token == '.') || (i == 0 && token == '-') ||
+                 (token == '-' && str[i - 1] == '(') || (token == '-' && str[i - 1] == ','))
             {
-                if (trimmedInput[i] == '-' && i == 0 || trimmedInput[i]=='-' && trimmedInput[i-1] == ',' || 
-                    trimmedInput[i] == '-' && trimmedInput[i-1] == '(')
+                number.Append(token);
+                if (i + 1< str.Length)
                 {
-                    number.Append(trimmedInput[i]);
-                    continue;
-                }
-
-                //if (trimmedInput[i] == '-')
-                //{
-                //    number.Length--;
-                //}
-                while (trimmedInput[i] >= '0' && trimmedInput[i] <= '9' || trimmedInput[i] == '.')
+                    if (!(str[i + 1] >= '0' && str[i + 1] <= '9') && str[i + 1] != '.')
                     {
+                        queue.Enqueue(number.ToString());
+                        number.Clear();
+                    } 
+                }
+            }
 
-                        number.Append(trimmedInput[i]);
-                        if (i + 1 < trimmedInput.Length)
-                            i++;
-                        else
-                            break;
+            if ((token >= '0' && token <= '9' && i == str.Length - 1))
+            {
+                number.Length--;
+                number.Append(token);
+                queue.Enqueue(number.ToString());
+                number.Clear();
+            }
+            // if token is '('
+            if (token == '(')
+            {
+                stack.Push(token.ToString());
+            }
 
-                    }
-                    queue.Enqueue(number.ToString());
-                    number.Clear();
+            // if token is ')'
+            if (token == ')')
+            {
+                while (stack.Peek() != "(")
+                {
+                    queue.Enqueue(stack.Pop());
+                }
                 
-            }
-
-            // if token is function
-            if (i + 1 < trimmedInput.Length)
-            {
-                if (functions.Contains(trimmedInput.Substring(i, 2).ToLower()))
-                {
-                    stack.Push(trimmedInput.Substring(i, 2).ToLower());
-                }
-            }
-
-            if (i + 2 < trimmedInput.Length)
-            {
-                if (functions.Contains(trimmedInput.Substring(i, 3).ToLower()))
-                {
-                    stack.Push(trimmedInput.Substring(i, 3).ToLower());
-                } 
-            }
-
-            if (i + 3 < trimmedInput.Length)
-            {
-                if (functions.Contains(trimmedInput.Substring(i, 4).ToLower()))
-                {
-                    stack.Push(trimmedInput.Substring(i, 4).ToLower());
-                } 
-            }
-
-            // if token is operator
-            if (operators.Contains(trimmedInput[i]) && i!= 0)
-            {
                 if (stack.Count != 0)
-	            {
-		            if (operators.Contains(stack.Peek()[0]) && (CalcPrecedence(trimmedInput[i]) <= CalcPrecedence(stack.Peek()[0])))
-                    {
-                        queue.Enqueue(stack.Pop());
-                    } 
-	            }
-
-                stack.Push(trimmedInput[i].ToString());
-            }
-            // if token is separator
-            if (trimmedInput[i] == ',')
-            {
-                while (stack.Count > 0 && stack.Peek() != "(")
                 {
-                    queue.Enqueue(stack.Pop());
-
-                    if (stack.Count == 1 && stack.Peek() != "(")
+                    if (functions.Contains(stack.Peek().ToString()))
                     {
-                        Console.WriteLine("Misplaced separator or mismatched parenthesis!");
-                        throw new ArgumentException();
+
+                        queue.Enqueue(stack.Pop());
+
                     }
-                }
-
-               
-            }
-
-            if (trimmedInput[i] == '(')
-            {
-                stack.Push(trimmedInput[i].ToString());
-            }
-
-            if (trimmedInput[i] == ')')
-            {
-                while (stack.Peek() != "(" && stack.Count != 0)
-                {
-                    queue.Enqueue(stack.Pop());
-                }
-                //stack.Pop();
-
-                if (stack.Count != 0)
-                {
-                    if (functions.Contains(stack.Peek()))
-                    {
-                        queue.Enqueue(stack.Pop());
-                    } 
                 }
 
                 if (stack.Count == 1 && stack.Peek() != "(")
                 {
-                    Console.WriteLine("Misplaced separator or mismatched parenthesis!");
-                    throw new ArgumentException();
+                    throw new ArgumentException("Misplased seperator or mismatched parenthense");
                 }
                 stack.Pop();
             }
+
+            // if token is a function
+            if (i + 2 < str.Length)
+                if (functions.Contains(str.Substring(i, 2)))
+                    stack.Push(str.Substring(i, 2));
+
+            if (i + 2 < str.Length)
+                if (functions.Contains(str.Substring(i, 3)))
+                    stack.Push(str.Substring(i, 3));
+                
+            if (i + 3 < str.Length)
+                if (functions.Contains(str.Substring(i, 4)))
+                    stack.Push(str.Substring(i, 4));
+            
+            // if token is argument separator - ,
+            if (token == ',')
+            {
+                while (stack.Peek() != "(")
+                {
+                    queue.Enqueue(stack.Pop());
+                    if (stack.Count == 1 && stack.Peek() != "(")
+                    {
+                        throw new ArgumentException("Misplased seperator or mismatched parenthense");
+                    }
+                }
+            }
+
+            // if token is operator
+            if (operators.Contains(token.ToString()))
+            {
+                if (operators.Contains(token.ToString()) && stack.Count!= 0)
+                {
+                    if (OperatorPrecedence(token) <= OperatorPrecedence(stack.Peek()[0]))
+                         queue.Enqueue(stack.Pop());
+                }
+
+                if (functions.Contains(token.ToString()) && stack.Count != 0)
+                {
+                    if (OperatorPrecedence(token) <= OperatorPrecedence(stack.Peek()[0]))
+                        queue.Enqueue(stack.Pop());
+                }
+                if ( i > 0)
+                {
+                    if (str[i - 1] != ',' && str[i - 1] != '(')
+                    {
+                        stack.Push(token.ToString());
+                    }
+                }
+            }
         }
 
-        while (stack.Count != 0)
+        if (stack.Count > 0)
         {
-            if(brackets.Contains(stack.Peek()[0]))
+            
+            while (stack.Count!= 0)
             {
-                Console.WriteLine("Missmatched parenthese!");
-                throw new ArgumentException();
+                if (stack.Peek() == "(" || stack.Peek() == ")")
+                {
+                    throw new ArgumentException("Mismatched parenthenses");
+                }
+                queue.Enqueue(stack.Pop());
             }
-            queue.Enqueue(stack.Pop());
+            
         }
 
         return queue;
     }
 
-    static string TrimInput(string input)
+    static string TrimInput(string str)
     {
-        string trimmedInput = input.Replace(" ", string.Empty);
-        trimmedInput = trimmedInput.Trim();
+        string result = "";
 
-        return trimmedInput;
+        result = str.Replace(" ", string.Empty);
+        result = result.Trim();
+        return result;
     }
 
-    static int CalcPrecedence(char op)
+    static int OperatorPrecedence(char op)
     {
         if (op == '+' || op == '-')
         {
             return 1;
         }
-
         if (op == '*' || op == '/')
         {
             return 2;
         }
-
+        if (op == 's' || op =='l')
+        {
+            return 3;
+        }
+        if (op == 'p')
+        {
+            return 3;
+        }
         return 0;
     }
 }
