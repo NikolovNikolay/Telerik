6b0423c283cbe1862bfa44ff2de23ae72d14c443
Nikolay Nikolov <nikolov89@gmail.com>
2013-12-15 02:19:01 +0200

    fixes

--- Data Structures and Algorithms/06.Data-Structures-Efficiency/03.BigDictionary/BigDictionary.cs
+++ Data Structures and Algorithms/06.Data-Structures-Efficiency/03.BigDictionary/BigDictionary.cs
@@ -9,20 +9,24 @@ namespace BigDictionary
     {
         private Dictionary<K1, V> firstHalfDictionary;
         private Dictionary<K2, V> secondHalfDictionary;
-        private Dictionary<KeyValuePair<K1, K2>, V> mainDictionary;
+        private Dictionary<Tuple<K1, K2>, V> mainDictionary; /* Here, instead of Tuple class, KeyValuePair struct could be used.
+                                                              * Both data structures have their benefits, but as if we need
+                                                              * passing data as arguments, return value, search - Tuple has better
+                                                              * performance. KeyValuePair is faster in allocationg its data
+                                                              */
 
         public BigDictionary()
         {
             this.firstHalfDictionary = new Dictionary<K1, V>();
             this.secondHalfDictionary = new Dictionary<K2, V>();
-            this.mainDictionary = new Dictionary<KeyValuePair<K1, K2>, V>();
+            this.mainDictionary = new Dictionary<Tuple<K1, K2>, V>();
         }
 
         public void AddElement(K1 key1,K2 key2,V value)
         {
             this.firstHalfDictionary.Add(key1, value);
             this.secondHalfDictionary.Add(key2, value);
-            this.mainDictionary.Add(new KeyValuePair<K1, K2>(key1, key2), value);
+            this.mainDictionary.Add(new Tuple<K1, K2>(key1, key2), value);
         }
 
         public V FindElementByItsPrimaryKey(K1 key)
@@ -37,7 +41,7 @@ namespace BigDictionary
 
         public V FindElementByBothKeys(K1 key1, K2 key2)
         {
-            var tempKvp = new KeyValuePair<K1, K2>(key1,key2);
+            var tempKvp = new Tuple<K1, K2>(key1,key2);
             return this.mainDictionary[tempKvp];
         }
     }
